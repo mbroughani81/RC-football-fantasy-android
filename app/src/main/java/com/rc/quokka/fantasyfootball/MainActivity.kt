c4ea8a1f177c774c1e0abf871f911cf0ec97668f
Mohammad Broughani <gitmbroughani81@gmail.com>
2022-08-22 20:35:13 +0430

    add view mode section view

--- app/src/main/java/com/rc/quokka/fantasyfootball/MainActivity.kt
+++ app/src/main/java/com/rc/quokka/fantasyfootball/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
@@ -28,6 +29,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.rc.quokka.fantasyfootball.ui.theme.FantasyFootballTheme
 import com.rc.quokka.fantasyfootball.ui.theme.VazirFont
 import org.intellij.lang.annotations.JdkConstants
@@ -42,6 +44,14 @@ class MainActivity : ComponentActivity() {
                 Column {
                     Header()
                     NavBar()
+                    Scaffold(
+                        topBar = {
+                            TopBar()
+                        }
+                    ) {
+
+                    }
+
                 }
             }
         }
@@ -108,7 +118,6 @@ fun NavBar() {
             .padding(top = 10.dp, start = 8.dp, end = 8.dp)
             .height(40.dp)
     ) {
-
         Box() {
             Text(
                 text = "تیم من",
@@ -131,21 +140,154 @@ fun NavBar() {
                 )
             }
         }
+        DropdownMenu(
+            expanded = expanded.value,
+            onDismissRequest = { expanded.value = false },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items.forEach {
+                DropdownMenuItem(onClick = { Log.d(TAG, it) }) {
+                    Box(modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()) {
+                        Text(
+                            text = it,
+                            fontFamily = VazirFont,
+                            fontSize = 14.sp,
+                            color = Color(0xff3D195B),
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                }
+            }
+        }
     }
-    DropdownMenu(
-        expanded = expanded.value,
-        onDismissRequest = { expanded.value = false },
+
+}
+
+@Composable
+fun TopBar() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxWidth()
     ) {
-        items.forEach {
-            DropdownMenuItem(onClick = { Log.d(TAG, it) }) {
-                Box(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
+        Card {
+            Box {
+                Image(
+                    painter = painterResource(id = R.drawable.rahnema_college_logo),
+                    contentDescription = "premier league icon",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .width(100.dp)
+                )
+            }
+        }
+        Card(
+            backgroundColor = Color(0xfff7f7f7)
+        ) {
+            Box(modifier = Modifier.padding(horizontal = 7.dp, vertical = 3.dp)) {
+                Row {
+                    TextButton(
+                        onClick = {},
+                    ) {
+                        Text(
+                            "مشاهده لیست",
+                            fontFamily = VazirFont,
+                            fontSize = 16.sp,
+                            color = Color(0xff3D195B),
+                            modifier = Modifier.padding(horizontal = 10.dp)
+                        )
+                    }
+                    TextButton(onClick = {}) {
+                        Text(
+                            "شماتیک ترکیب",
+                            fontFamily = VazirFont,
+                            fontSize = 16.sp,
+                            color = Color(0xff3D195B),
+                            modifier = Modifier
+                                .padding(horizontal = 10.dp)
+                        )
+                    }
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(35.dp))
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(30.dp),
+            modifier = Modifier
+                .height(70.dp),
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 30.dp)
+                    .clip(RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp))
+                    .background(brush = Brush.horizontalGradient(colors = listOf(Color(0xff04f7da), Color(0xff02fda2))))
+                    .padding(5.dp)
+                    .fillMaxHeight()
+            )
+            {
+                Text(
+                    "۱۵/۱۲",
+                    fontFamily = VazirFont,
+                    fontSize = 16.sp,
+                    color = Color(0xff3D195B),
+                    modifier = Modifier
+                )
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(
+                        painter = painterResource(id = R.drawable.head),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier
+                            .width(50.dp)
+                    )
                     Text(
-                        text = it,
+                        "بازیکن باقی مانده",
                         fontFamily = VazirFont,
-                        fontSize = 14.sp,
+                        fontSize = 12.sp,
                         color = Color(0xff3D195B),
-                        modifier = Modifier.align(Alignment.Center)
+                        modifier = Modifier
+                            .wrapContentWidth(unbounded = true)
+                    )
+                }
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 30.dp)
+                    .clip(RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp))
+                    .background(brush = Brush.horizontalGradient(colors = listOf(Color(0xff04f7da), Color(0xff02fda2))))
+                    .padding(5.dp)
+                    .fillMaxHeight()
+            ) {
+                Text(
+                    "۷۳",
+                    fontFamily = VazirFont,
+                    fontSize = 16.sp,
+                    color = Color(0xff3D195B),
+                    modifier = Modifier
+                )
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(
+                        painter = painterResource(id = R.drawable.wallet),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier
+                            .width(50.dp)
+                    )
+                    Text(
+                        "پول باقی مانده",
+                        fontFamily = VazirFont,
+                        fontSize = 12.sp,
+                        color = Color(0xff3D195B),
+                        modifier = Modifier
+                            .wrapContentWidth(unbounded = true)
                     )
                 }
             }
